a9ca7d83f3eed1d6b04ccf4b32a5f2ab697300f5
thuyencode <thuyencode@proton.me>
2023-12-09 14:49:53 +0700

    Submit solution: custom-signs

--- javascript/custom-signs/custom-signs.js
+++ javascript/custom-signs/custom-signs.js
@@ -14,7 +14,7 @@
  */
 
 export function buildSign(occasion, name) {
-  throw new Error('Implement the buildSign function');
+  return `Happy ${occasion} ${name}!`
 }
 
 /**
@@ -26,7 +26,9 @@ export function buildSign(occasion, name) {
  */
 
 export function buildBirthdaySign(age) {
-  throw new Error('Implement the buildBirthdaySign function');
+  return `Happy Birthday! What a ${
+    age >= 50 ? 'mature' : 'young'
+  } fellow you are.`
 }
 
 /**
@@ -39,7 +41,7 @@ export function buildBirthdaySign(age) {
  */
 
 export function graduationFor(name, year) {
-  throw new Error('Implement the graduationFor function');
+  return `Congratulations ${name}!\nClass of ${year}`
 }
 
 /**
@@ -53,5 +55,5 @@ export function graduationFor(name, year) {
  */
 
 export function costOf(sign, currency) {
-  throw new Error('Implement the costOf function');
+  return `Your sign costs ${sign.length * 2 + 20}.00 ${currency}.`
 }
