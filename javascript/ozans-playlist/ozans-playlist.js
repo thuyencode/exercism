e1cc4c592dd45a686ed1c618e42d545902c56c6f
thuyencode <thuyencode@proton.me>
2023-12-11 22:39:20 +0700

    Submit solution: ozans-playlist

--- javascript/ozans-playlist/ozans-playlist.js
+++ javascript/ozans-playlist/ozans-playlist.js
@@ -11,7 +11,7 @@
  * @returns {string[]} new playlist with unique entries
  */
 export function removeDuplicates(playlist) {
-  throw new Error('Please implement the removeDuplicates function');
+  return [...new Set(playlist)]
 }
 
 /**
@@ -22,7 +22,7 @@ export function removeDuplicates(playlist) {
  * @returns {boolean} whether the track is in the playlist
  */
 export function hasTrack(playlist, track) {
-  throw new Error('Please implement the hasTrack function');
+  return new Set(playlist).has(track)
 }
 
 /**
@@ -33,7 +33,7 @@ export function hasTrack(playlist, track) {
  * @returns {string[]} new playlist
  */
 export function addTrack(playlist, track) {
-  throw new Error('Please implement the addTrack function');
+  return [...new Set(playlist).add(track)]
 }
 
 /**
@@ -44,7 +44,10 @@ export function addTrack(playlist, track) {
  * @returns {string[]} new playlist
  */
 export function deleteTrack(playlist, track) {
-  throw new Error('Please implement the deleteTrack function');
+  const set = new Set(playlist)
+  set.delete(track)
+
+  return [...set]
 }
 
 /**
@@ -54,5 +57,7 @@ export function deleteTrack(playlist, track) {
  * @returns {string[]} list of artists
  */
 export function listArtists(playlist) {
-  throw new Error('Please implement the listArtists function');
+  return [
+    ...new Set(removeDuplicates(playlist).map((track) => track.split(' - ')[1]))
+  ]
 }
