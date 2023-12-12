a98c26f37917cad6e387596fb5c96a20d7397f2f
thuyencode <thuyencode@proton.me>
2023-12-12 08:53:34 +0700

    Change solution: two-fer
    Set default value for param "name"

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -8,6 +8,6 @@
  *
  * @param {string} name
  */
-export const twoFer = (name) => {
-  return `One for ${name || 'you'}, one for me.`
+export const twoFer = (name = 'you') => {
+  return `One for ${name}, one for me.`
 }
