31953f137f82d4585f1eefe566fd7a30bdd672da
thuyencode <thuyencode@proton.me>
2023-12-14 19:53:57 +0700

    Submit solution: bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -3,6 +3,56 @@
 // convenience to get you started writing code faster.
 //
 
+/**
+ * Get Bob's reply to `message
+ * `
+ * @param {string} message
+ * @returns {string}
+ */
 export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
-};
+  const trimmedMessage = message.trim()
+
+  if (isAllCapital(trimmedMessage) && trimmedMessage.endsWith('?')) {
+    return "Calm down, I know what I'm doing!"
+  }
+
+  if (isAllCapital(trimmedMessage)) {
+    return 'Whoa, chill out!'
+  }
+
+  if (trimmedMessage.endsWith('?')) {
+    return 'Sure.'
+  }
+
+  if (isSilence(trimmedMessage)) {
+    return 'Fine. Be that way!'
+  }
+
+  return 'Whatever.'
+}
+
+/**
+ * Check if `message` only contains capital letters
+ *
+ * @param {string} message
+ * @returns {boolean}
+ */
+function isAllCapital(message) {
+  const str = message.replace(/[^a-z]/gi, '')
+
+  if (str.length === 0) return false
+
+  return [...'abcdefghijklmnopqrstuvwxyz'].every((char) => !str.includes(char))
+}
+
+/**
+ * Check if `message` is silence or not
+ *
+ * @param {string} message
+ * @returns {boolean}
+ */
+function isSilence(message) {
+  if (message.length === 0) return true
+
+  return /^[\s\\]*$/.test(message.trim())
+}
