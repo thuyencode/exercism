1e139a8bcf5b3241e86b1762ffba192a035f6ff4
thuyencode <thuyencode@proton.me>
2023-12-17 16:25:27 +0700

    Submit solution: list-ops

--- javascript/list-ops/list-ops.js
+++ javascript/list-ops/list-ops.js
@@ -4,39 +4,142 @@
 //
 
 export class List {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  #list = []
+
+  /**
+   * @constructor
+   * @param {any[]} list
+   */
+  constructor(list = []) {
+    this.#list = list
   }
 
-  append() {
-    throw new Error('Remove this statement and implement this function');
+  /**
+   * Add all items in `list` to the end of the list
+   *
+   * @param {List} list
+   * @returns {List}
+   */
+  append(list) {
+    this.#list = [...this.values, ...list.values]
+
+    return this
   }
 
-  concat() {
-    throw new Error('Remove this statement and implement this function');
+  /**
+   *
+   * @param {List} lists
+   * @returns {List}
+   */
+  concat(lists) {
+    if (lists === undefined) return this
+
+    if (lists.length() > 0) {
+      for (const item of lists.values) {
+        if (item instanceof List) {
+          this.append(item)
+        } else {
+          this.#list = [...this.values, ...item]
+        }
+      }
+    }
+
+    return this
   }
 
-  filter() {
-    throw new Error('Remove this statement and implement this function');
+  /**
+   * Return the list of all items for which `callback(item)` is `true`
+   *
+   * @param {Function} callback
+   * @returns {List}
+   */
+  filter(callback) {
+    let temp = []
+
+    for (const item of this.values) {
+      if (callback(item)) {
+        temp = [...temp, item]
+      }
+    }
+
+    return new List(temp)
   }
 
-  map() {
-    throw new Error('Remove this statement and implement this function');
+  /**
+   * Return the list of the results of applying `callback(item)` on all items
+   *
+   * @param {Function} callback
+   * @returns {List}
+   */
+  map(callback) {
+    let temp = []
+
+    for (const item of this.values) {
+      temp = [...temp, callback(item)]
+    }
+
+    return new List(temp)
   }
 
+  /**
+   * Return the total number of items within the list
+   *
+   * @returns {number}
+   */
   length() {
-    throw new Error('Remove this statement and implement this function');
+    let count = 0
+
+    for (const index in this.values) {
+      count++
+    }
+
+    return count
   }
 
-  foldl() {
-    throw new Error('Remove this statement and implement this function');
+  /**
+   * Reduce each item into the `accumulator` from the left
+   *
+   * @param {Function} callback
+   * @param {number} accumulator
+   */
+  foldl(callback, accumulator) {
+    for (const item of this.values) {
+      accumulator = callback(accumulator, item)
+    }
+
+    return accumulator
   }
 
-  foldr() {
-    throw new Error('Remove this statement and implement this function');
+  /**
+   * Reduce each item into the `accumulator` from the right
+   *
+   * @param {Function} callback
+   * @param {number} accumulator
+   */
+  foldr(callback, accumulator) {
+    for (const item of this.reverse().values) {
+      accumulator = callback(accumulator, item)
+    }
+
+    return accumulator
   }
 
+  /**
+   * Return a list with all the original items, but in reversed order
+   *
+   * @returns {List}
+   */
   reverse() {
-    throw new Error('Remove this statement and implement this function');
+    let temp = []
+
+    for (const item of this.values) {
+      temp = [item, ...temp]
+    }
+
+    return new List(temp)
+  }
+
+  get values() {
+    return this.#list
   }
 }
