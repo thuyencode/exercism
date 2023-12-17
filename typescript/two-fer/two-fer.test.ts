8f9a40f7f4ef6651cb1f219975ad603c8eb7c0b7
thuyencode <thuyencode@proton.me>
2023-12-17 21:35:59 +0700

    Submit solution: typescript/two-fer

--- typescript/two-fer/two-fer.test.ts
+++ typescript/two-fer/two-fer.test.ts
@@ -1,17 +1,18 @@
+import { describe, expect, test } from 'bun:test'
 import { twoFer } from './two-fer'
 
 describe('TwoFer', () => {
-  it('no name given', () => {
+  test('no name given', () => {
     const expected = 'One for you, one for me.'
     expect(twoFer()).toEqual(expected)
   })
 
-  xit('a name given', () => {
+  test('a name given', () => {
     const expected = 'One for Alice, one for me.'
     expect(twoFer('Alice')).toEqual(expected)
   })
 
-  xit('another name given', () => {
+  test('another name given', () => {
     const expected = 'One for Bob, one for me.'
     expect(twoFer('Bob')).toEqual(expected)
   })
