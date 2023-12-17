6c53dd0cd69029f11be2851f0f96b8380bac2e9d
thuyencode <thuyencode@proton.me>
2023-12-17 18:55:15 +0700

    Submit solution: robot-name

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,6 +1,49 @@
 // This is only a SKELETON file for the 'Robot Name' exercise. It's been
 // provided as a convenience to get your started writing code faster.
 
-export class Robot {}
+export class Robot {
+  /**
+   * @type {string[]}
+   */
+  #names = []
 
-Robot.releaseNames = () => {};
+  constructor() {
+    this.#names.unshift(this.#getRandomName())
+  }
+
+  get alphabets() {
+    return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+
+  get name() {
+    return this.#names[0]
+  }
+
+  reset() {
+    let newName = this.#getRandomName()
+
+    while (this.#names.includes(newName)) {
+      newName = this.#getRandomName()
+    }
+
+    this.#names.unshift(newName)
+  }
+
+  #getRandomName() {
+    const len = this.alphabets.length
+    const n = 10
+
+    return `${this.alphabets[this.#randomNum(len)]}${
+      this.alphabets[this.#randomNum(len)]
+    }${this.#randomNum(n)}${this.#randomNum(n)}${this.#randomNum(n)}`
+  }
+
+  /**
+   * @param {number} n
+   */
+  #randomNum(n) {
+    return Math.floor(Math.random() * n)
+  }
+}
+
+Robot.releaseNames = () => {}
