4dd5d41564209ff1135c671fc394a9759d89cd5d
thuyencode <thuyencode@proton.me>
2023-12-20 20:59:23 +0700

    Submit solution: typescript/rna-transcription

--- typescript/rna-transcription/rna-transcription.test.ts
+++ typescript/rna-transcription/rna-transcription.test.ts
@@ -1,35 +1,37 @@
+// @ts-nocheck
+
 import { toRna } from './rna-transcription'
 
 describe('Transcriptor', () => {
-  it('transcribes cytosine to guanine', () => {
+  test('transcribes cytosine to guanine', () => {
     expect(toRna('C')).toEqual('G')
   })
 
-  xit('transcribes guanine to cytosine', () => {
+  test('transcribes guanine to cytosine', () => {
     expect(toRna('G')).toEqual('C')
   })
 
-  xit('transcribes adenine to uracil', () => {
+  test('transcribes adenine to uracil', () => {
     expect(toRna('A')).toEqual('U')
   })
 
-  xit('transcribes thymine to adenine', () => {
+  test('transcribes thymine to adenine', () => {
     expect(toRna('T')).toEqual('A')
   })
 
-  xit('transcribes all dna nucleotides to their rna complements', () => {
+  test('transcribes all dna nucleotides to their rna complements', () => {
     expect(toRna('ACGTGGTCTTAA')).toEqual('UGCACCAGAAUU')
   })
 
-  xit('correctly handles invalid input', () => {
-    expect(() => toRna('U')).toThrowError('Invalid input DNA.')
+  test('correctly handles invalid input', () => {
+    expect(() => toRna('U')).toThrow('Invalid input DNA.')
   })
 
-  xit('correctly handles completely invalid input', () => {
-    expect(() => toRna('XXX')).toThrowError('Invalid input DNA.')
+  test('correctly handles completely invalid input', () => {
+    expect(() => toRna('XXX')).toThrow('Invalid input DNA.')
   })
 
-  xit('correctly handles partially invalid input', () => {
-    expect(() => toRna('ACGTXXXCTTAA')).toThrowError('Invalid input DNA.')
+  test('correctly handles partially invalid input', () => {
+    expect(() => toRna('ACGTXXXCTTAA')).toThrow('Invalid input DNA.')
   })
 })
--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,3 +1,27 @@
-export function toRna() {
-  throw new Error('Remove this statement and implement this function')
+export function toRna(dna: string) {
+  if (!dna) {
+    throw new Error('Invalid input DNA.')
+  }
+
+  return dna
+    .split('')
+    .map((nucleotide) => {
+      const rna = DNA_TO_RNA.get(nucleotide)
+
+      if (!rna) {
+        throw new Error('Invalid input DNA.')
+      }
+
+      return rna
+    })
+    .join('')
 }
+
+const DNA_TO_RNA = Object.freeze(
+  new Map<string, string>([
+    ['G', 'C'],
+    ['C', 'G'],
+    ['T', 'A'],
+    ['A', 'U']
+  ])
+)
