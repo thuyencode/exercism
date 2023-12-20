4a54343f4dd048a10b338c2eacd6cb2bec1b21ef
thuyencode <thuyencode@proton.me>
2023-12-20 20:49:09 +0700

    Submit solution: typescript/resistor-color-trio

--- typescript/resistor-color-trio/resistor-color-trio.ts
+++ typescript/resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,34 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+// Thanks to: https://exercism.org/tracks/typescript/exercises/resistor-color-trio/solutions/bobahop
+
+export function decodedResistorValue([r1, r2, r3]: Color[]) {
+  const ohms =
+    (COLORS.indexOf(r1) * 10 + COLORS.indexOf(r2)) *
+    Math.pow(10, COLORS.indexOf(r3))
+
+  const [divisor, prefix] = METRIC_PREFIXES.find(
+    ([divisor]) => ohms >= divisor
+  ) ?? [1, '']
+
+  return `${ohms / divisor} ${prefix}ohms`
 }
+
+type Color = (typeof COLORS)[number]
+
+const COLORS = Object.freeze([
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+] as const)
+
+const METRIC_PREFIXES = Object.freeze([
+  [1_000_000_000, 'giga'],
+  [1_000_000, 'mega'],
+  [1000, 'kilo']
+] as const)
