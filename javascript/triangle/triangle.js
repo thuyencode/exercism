a74510074d75ef1de91504550221441107220d2b
thuyencode <thuyencode@proton.me>
2024-01-06 15:46:34 +0700

    Submit solution: javascript/triangle

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -4,19 +4,41 @@
 //
 
 export class Triangle {
+  #sides
+
+  /**
+   * @param  {...number} sides
+   */
   constructor(...sides) {
-    throw new Error('Remove this statement and implement this function');
+    this.#sides = sides
+  }
+
+  get isTriangle() {
+    if (!this.#sides.every((side) => side > 0)) {
+      return false
+    }
+
+    const [a, b, c] = this.#sides
+
+    return a + b >= c && b + c >= a && a + c >= b
   }
 
   get isEquilateral() {
-    throw new Error('Remove this statement and implement this function');
+    if (!this.isTriangle) return false
+
+    return new Set(this.#sides).size === 1
   }
 
   get isIsosceles() {
-    throw new Error('Remove this statement and implement this function');
+    if (!this.isTriangle) return false
+    if (this.isEquilateral) return true
+
+    return new Set(this.#sides).size === 2
   }
 
   get isScalene() {
-    throw new Error('Remove this statement and implement this function');
+    if (!this.isTriangle) return false
+
+    return !(this.isEquilateral || this.isIsosceles)
   }
 }
