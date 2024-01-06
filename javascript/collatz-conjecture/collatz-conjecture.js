d11709cee1f59597d1a4f47edcc92b8a6eae6575
thuyencode <thuyencode@proton.me>
2024-01-06 11:35:02 +0700

    Submit solution: javascript/collatz-conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+/**
+ * Solve The Collatz Conjecture or 3x+1 problem
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+export const steps = (n) => {
+  if (n <= 0) {
+    throw new Error('Only positive numbers are allowed')
+  }
+
+  let count = 0
+
+  while (n > 1) {
+    if (n % 2 === 0) n /= 2
+    else n = 3 * n + 1
+
+    count++
+  }
+
+  return count
+}
