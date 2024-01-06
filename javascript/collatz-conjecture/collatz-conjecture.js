c540af6330e998adcdb0bd90bcb7712722b90356
thuyencode <thuyencode@proton.me>
2024-01-06 11:38:10 +0700

    Change solution: javascript/collatz-conjecture

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -9,19 +9,12 @@
  * @param {number} n
  * @returns {number}
  */
-export const steps = (n) => {
+export const steps = (n, count = 0) => {
   if (n <= 0) {
     throw new Error('Only positive numbers are allowed')
   }
 
-  let count = 0
+  if (n === 1) return count
 
-  while (n > 1) {
-    if (n % 2 === 0) n /= 2
-    else n = 3 * n + 1
-
-    count++
-  }
-
-  return count
+  return steps(n % 2 === 0 ? n / 2 : 3 * n + 1, ++count)
 }
