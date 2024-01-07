435ff2417d3cab10ddaa515e4c3c3ca18f52410d
thuyencode <thuyencode@proton.me>
2024-01-07 16:41:35 +0700

    Submit solution: javascript/clock

--- javascript/clock/clock.js
+++ javascript/clock/clock.js
@@ -2,25 +2,62 @@
 // This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// I'm dumb at math so thanks to Michael Brig: https://youtu.be/MD_MFDpHtJ0
 
 export class Clock {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  #hours = 0
+  #minutes = 0
+
+  /**
+   * @param {number} hours
+   * @param {number} minutes
+   */
+  constructor(hours, minutes = 0) {
+    this.#hours = hours
+    this.#minutes = minutes
+    this.convert()
+  }
+
+  get hours() {
+    return this.#hours
+  }
+
+  get minutes() {
+    return this.#minutes
+  }
+
+  convert() {
+    this.#hours += Math.floor(this.#minutes / 60)
+    this.#hours =
+      this.#hours % 24 < 0 ? 24 + (this.#hours % 24) : this.#hours % 24
+    this.#minutes =
+      this.#minutes % 60 < 0 ? 60 + (this.#minutes % 60) : this.#minutes % 60
   }
 
   toString() {
-    throw new Error('Remove this statement and implement this function');
+    return `${this.#hours < 10 ? 0 : ''}${this.#hours}:${
+      this.#minutes < 10 ? 0 : ''
+    }${this.#minutes}`
   }
 
-  plus() {
-    throw new Error('Remove this statement and implement this function');
+  plus(minutes = 1) {
+    this.#minutes += minutes
+    this.convert()
+
+    return this
   }
 
-  minus() {
-    throw new Error('Remove this statement and implement this function');
+  minus(minutes = 1) {
+    this.#minutes -= minutes
+    this.convert()
+
+    return this
   }
 
-  equals() {
-    throw new Error('Remove this statement and implement this function');
+  /**
+   * @param {Clock} clock
+   */
+  equals(clock) {
+    return this.#hours === clock.hours && this.#minutes === clock.minutes
   }
 }
