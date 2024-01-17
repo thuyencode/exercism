6449f1e20aadbc35b979eb699316b282097765be
Thuyen Code <thuyencode@proton.me>
2024-01-17 16:22:59 +0700

    Submit solution: python/leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,12 @@
-def leap_year(year):
-    pass
+"""
+Python Exercism: Leap
+"""
+
+def leap_year(year: int) -> bool:
+    """
+    Determine whether a given year is a leap year.
+
+    :param year: int - the year to check
+    :return: bool - True if the year is a leap year, False otherwise
+    """
+    return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
