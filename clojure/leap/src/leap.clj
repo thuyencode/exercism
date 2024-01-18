f65c133ad9510656e831be64b4396ec3aa2af0f9
Thuyen Code <thuyencode@proton.me>
2024-01-18 10:41:59 +0700

    Submit solution: clojure/cars-assemble

--- clojure/leap/src/leap.clj
+++ clojure/leap/src/leap.clj
@@ -1,5 +1,13 @@
+;; Clojure hurts my brain so much!
+;; Thanks to: https://youtu.be/Fj5m16is5hI
+
 (ns leap)
+
+(defn divisible-by? [x y]
+  (zero? (rem x y)))
 
 (defn leap-year? [year] ;; <- argslist goes here
   ;; your code goes here
-)
+  (and (divisible-by? year 4)
+    (or (not (divisible-by? year 100))
+      (divisible-by? year 400))))
