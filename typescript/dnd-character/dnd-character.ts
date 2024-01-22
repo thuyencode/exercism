47aeed4787ff21aca84d00f0d272d11a87cb7157
Thuyen Code <thuyencode@proton.me>
2024-01-22 16:01:52 +0700

    Submit solution: typescript/dnd-character

--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,39 @@
+// Thanks to: https://exercism.org/tracks/typescript/exercises/dnd-character/solutions/gabrielecanepa
+
 export class DnDCharacter {
-  public static generateAbilityScore(): number {
-    throw new Error('Remove this statement and implement this function')
+  readonly strength: number
+  readonly dexterity: number
+  readonly constitution: number
+  readonly intelligence: number
+  readonly wisdom: number
+  readonly charisma: number
+  readonly hitpoints: number
+
+  constructor() {
+    this.strength = DnDCharacter.generateAbilityScore()
+    this.dexterity = DnDCharacter.generateAbilityScore()
+    this.constitution = DnDCharacter.generateAbilityScore()
+    this.intelligence = DnDCharacter.generateAbilityScore()
+    this.wisdom = DnDCharacter.generateAbilityScore()
+    this.charisma = DnDCharacter.generateAbilityScore()
+    this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution)
   }
 
-  public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this statement and implement this function')
+  public static generateAbilityScore(): number {
+    const rolls: number[] = [...Array(4)].map((_) =>
+      this.generateRandomDiceRollScore()
+    )
+    return rolls
+      .sort((a, b) => a - b)
+      .slice(0, 3)
+      .reduce((n, sum) => sum + n)
+  }
+
+  public static getModifierFor(numberValue: number): number {
+    return Math.floor((numberValue - 10) / 2)
+  }
+
+  private static generateRandomDiceRollScore(): number {
+    return Math.floor(Math.random() * 6) + 1
   }
 }
