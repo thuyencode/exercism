dab202f23f50bcf69940690274a93219799b3ed4
Thuyen Code <thuyencode@proton.me>
2024-01-22 15:19:32 +0700

    Submit solution: typescript/space-age

--- typescript/space-age/space-age.ts
+++ typescript/space-age/space-age.ts
@@ -1,3 +1,22 @@
-export function age(planet: unknown, seconds: unknown): unknown {
-  throw new Error('Remove this statement and implement this function')
+export function age(
+  planet: keyof typeof A_YEAR_ON_PLANET,
+  seconds: number
+): number {
+  return Number(
+    (seconds / (A_YEAR_ON_PLANET[planet] * A_YEAR_ON_EARTH)).toFixed(2)
+  )
+}
+
+// Seconds
+const A_YEAR_ON_EARTH = 31557600
+
+const A_YEAR_ON_PLANET = {
+  mercury: 0.2408467,
+  venus: 0.61519726,
+  earth: 1,
+  mars: 1.8808158,
+  jupiter: 11.862615,
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
 }
