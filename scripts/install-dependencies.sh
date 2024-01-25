6016b6d17b92024e2a4025ecaf8d3bdc6c6b8973
Thuyen Code <thuyencode@proton.me>
2024-01-26 00:45:45 +0700

    Add support for Nim lang

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -6,13 +6,13 @@ read -p "Enter your Exercism's API key: " api_key
 if [ -n "$api_key" ]; then
   exercism configure --token=$api_key -w /workspaces/exercism
 else
+  exercism configure -w /workspaces/exercism
   echo "You need to get the API key at https://exercism.org/settings/api_cli to use the exercism CLI!"
 fi
 
-sudo apt update
-sudo apt full-upgrade -y
-sudo apt install python3-pytest cmake make leiningen clangd-16 nodejs -y
+sudo apt install python3-pytest cmake make leiningen clangd-16 nodejs nim -y
 sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-16 100
+
 curl -fsSL https://bun.sh/install | bash
 source /home/vscode/.bashrc
 
