20f8cf3d060bb2f664e9a4a017241ce6988c0a44
Thuyen Code <thuyencode@proton.me>
2024-01-25 16:32:10 +0000

    Submit solution: cpp/reverse-string

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,14 @@
 #include "reverse_string.h"
+#include <string>
 
 namespace reverse_string {
+std::string reverse_string(std::string str) {
+  std::string new_string = "";
 
-}  // namespace reverse_string
+  for (char c : str) {
+    new_string = c + new_string;
+  }
+
+  return new_string;
+}
+} // namespace reverse_string
