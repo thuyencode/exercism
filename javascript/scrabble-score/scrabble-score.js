54a4d7667e3b1e3305d544022427183ae3cdf029
Thuyen Code <thuyencode@proton.me>
2024-01-29 03:17:47 +0000

    Submit solution: javascript/scrabble-score

--- javascript/scrabble-score/scrabble-score.js
+++ javascript/scrabble-score/scrabble-score.js
@@ -3,6 +3,50 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  throw new Error('Remove this statement and implement this function');
-};
+/**
+ * Compute the Scrabble score for `word`.
+ *
+ * @param {string} word
+ */
+export const score = (word) => {
+  word = word.toUpperCase()
+
+  return Array.from(LETTER_VALUES.keys()).reduce(
+    (acc, letter) =>
+      (word.match(new RegExp(letter, 'g')) || []).length *
+        LETTER_VALUES.get(letter) +
+      acc,
+    0
+  )
+}
+
+const LETTER_VALUES = Object.freeze(
+  new Map([
+    ['A', 1],
+    ['E', 1],
+    ['I', 1],
+    ['O', 1],
+    ['U', 1],
+    ['L', 1],
+    ['N', 1],
+    ['R', 1],
+    ['S', 1],
+    ['T', 1],
+    ['D', 2],
+    ['G', 2],
+    ['B', 3],
+    ['C', 3],
+    ['M', 3],
+    ['P', 3],
+    ['F', 4],
+    ['H', 4],
+    ['V', 4],
+    ['W', 4],
+    ['Y', 4],
+    ['K', 5],
+    ['J', 8],
+    ['X', 8],
+    ['Q', 10],
+    ['Z', 10]
+  ])
+)
