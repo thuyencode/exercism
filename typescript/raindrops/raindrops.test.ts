13b6589599064fd6882f9e51b1e942d6b46bf8a2
Thuyen Code <thuyencode@proton.me>
2024-02-04 10:12:16 +0000

    Submit solution: typescript/raindrops

--- typescript/raindrops/raindrops.test.ts
+++ typescript/raindrops/raindrops.test.ts
@@ -1,35 +1,35 @@
 import { convert } from './raindrops'
 
 describe('Raindrops', () => {
-  it('converts 1', () => expect(convert(1)).toEqual('1'))
+  test('converts 1', () => expect(convert(1)).toEqual('1'))
 
-  xit('converts 3', () => expect(convert(3)).toEqual('Pling'))
+  test('converts 3', () => expect(convert(3)).toEqual('Pling'))
 
-  xit('converts 5', () => expect(convert(5)).toEqual('Plang'))
+  test('converts 5', () => expect(convert(5)).toEqual('Plang'))
 
-  xit('converts 7', () => expect(convert(7)).toEqual('Plong'))
+  test('converts 7', () => expect(convert(7)).toEqual('Plong'))
 
-  xit('converts 6', () => expect(convert(6)).toEqual('Pling'))
+  test('converts 6', () => expect(convert(6)).toEqual('Pling'))
 
-  xit('converts 9', () => expect(convert(9)).toEqual('Pling'))
+  test('converts 9', () => expect(convert(9)).toEqual('Pling'))
 
-  xit('converts 10', () => expect(convert(10)).toEqual('Plang'))
+  test('converts 10', () => expect(convert(10)).toEqual('Plang'))
 
-  xit('converts 14', () => expect(convert(14)).toEqual('Plong'))
+  test('converts 14', () => expect(convert(14)).toEqual('Plong'))
 
-  xit('converts 15', () => expect(convert(15)).toEqual('PlingPlang'))
+  test('converts 15', () => expect(convert(15)).toEqual('PlingPlang'))
 
-  xit('converts 21', () => expect(convert(21)).toEqual('PlingPlong'))
+  test('converts 21', () => expect(convert(21)).toEqual('PlingPlong'))
 
-  xit('converts 25', () => expect(convert(25)).toEqual('Plang'))
+  test('converts 25', () => expect(convert(25)).toEqual('Plang'))
 
-  xit('converts 35', () => expect(convert(35)).toEqual('PlangPlong'))
+  test('converts 35', () => expect(convert(35)).toEqual('PlangPlong'))
 
-  xit('converts 49', () => expect(convert(49)).toEqual('Plong'))
+  test('converts 49', () => expect(convert(49)).toEqual('Plong'))
 
-  xit('converts 52', () => expect(convert(52)).toEqual('52'))
+  test('converts 52', () => expect(convert(52)).toEqual('52'))
 
-  xit('converts 105', () => expect(convert(105)).toEqual('PlingPlangPlong'))
+  test('converts 105', () => expect(convert(105)).toEqual('PlingPlangPlong'))
 
-  xit('converts 12121', () => expect(convert(12121)).toEqual('12121'))
+  test('converts 12121', () => expect(convert(12121)).toEqual('12121'))
 })
--- typescript/raindrops/raindrops.ts
+++ typescript/raindrops/raindrops.ts
@@ -1,3 +1,19 @@
-export function convert() {
-  throw new Error('Remove this statement and implement this function')
+// Based on my solution at the JavaScript track: https://exercism.org/tracks/javascript/exercises/raindrops/solutions/thuyencode
+
+export function convert(num: number): string {
+  if (num <= 1) return String(num)
+
+  const raindrops = Object.keys(RAINDROPS).reduce(
+    (acc, factor) =>
+      num % Number(factor) === 0 ? acc + RAINDROPS[factor] : acc,
+    ''
+  )
+
+  return raindrops.length !== 0 ? raindrops : String(num)
 }
+
+const RAINDROPS: Record<string, string> = Object.freeze({
+  '3': 'Pling',
+  '5': 'Plang',
+  '7': 'Plong'
+})
