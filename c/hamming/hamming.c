e96e882cfae7466d4360be3469ec01f3efed4b6f
Thuyen Code <thuyencode@proton.me>
2024-02-08 16:09:10 +0000

    Submit solution: c/hamming

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,1 +1,21 @@
 #include "hamming.h"
+#include <string.h>
+// Based on my solution using JavaScript: https://exercism.org/tracks/javascript/exercises/hamming/solutions/thuyencode
+
+int compute(const char *lhs, const char *rhs)
+{
+  if (strlen(lhs) != strlen(rhs))
+  {
+    return -1;
+  }
+
+  int count = 0;
+
+  for (size_t i = 0; i < strlen(lhs); i++)
+  {
+    if (lhs[i] != rhs[i])
+      count++;
+  }
+
+  return count;
+}
