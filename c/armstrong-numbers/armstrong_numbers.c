a5dd7d2349dd54e6279e59db7bb0b34ca87e1117
Thuyen Code <thuyencode@proton.me>
2024-02-08 15:30:35 +0000

    Submit solution: c/armstrong-numbers

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,1 +1,32 @@
 #include "armstrong_numbers.h"
+// Thanks to: https://en.wikipedia.org/wiki/Narcissistic_number#C
+
+bool is_armstrong_number(int candidate)
+{
+  int temp = candidate;
+  int number_of_digits = 0;
+
+  // Calculate the number on digits in `candidate`
+  while (temp != 0)
+  {
+    temp /= 10;
+    number_of_digits++;
+  }
+
+  int sum = 0;
+
+  for (int i = candidate; i != 0; i /= 10)
+  {
+    int num = i % 10;
+    int n = 1;
+
+    for (int j = 0; j < number_of_digits; j++)
+    {
+      n *= num;
+    }
+
+    sum += n;
+  }
+
+  return sum == candidate;
+}
