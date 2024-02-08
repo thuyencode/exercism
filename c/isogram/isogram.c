dcccc13eb0a962568b4878eafc812c970b0b8c79
Thuyen Code <thuyencode@proton.me>
2024-02-08 15:41:25 +0000

    Submit solution: c/isogram

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,1 +1,25 @@
 #include "isogram.h"
+#include <string.h>
+
+bool is_isogram(const char phrase[])
+{
+  const int length = strlen(phrase);
+
+  for (int i = 0; i < length; i++)
+  {
+    const char temp = phrase[i];
+
+    for (int j = 0; j < length; j++)
+    {
+      if (j == i)
+        continue;
+
+      if (phrase[j] == temp)
+      {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
