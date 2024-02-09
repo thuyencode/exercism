c73d6b623d796999deb0126d994f26cf79f345ce
Thuyen Code <thuyencode@proton.me>
2024-02-09 14:43:43 +0000

    Submit solution: c/binary-search

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -1,1 +1,24 @@
 #include "binary_search.h"
+
+const int *binary_search(int value, const int *arr, size_t length)
+{
+  if (!arr)
+    return NULL;
+
+  size_t left = 0, right = length;
+
+  while (left < right)
+  {
+    size_t mid = left + ((right - left) / 2);
+    int look = arr[mid];
+
+    if (look == value)
+      return (int *)&arr[mid];
+    if (look < value)
+      left = mid + 1;
+    else
+      right = mid;
+  }
+
+  return NULL;
+}
