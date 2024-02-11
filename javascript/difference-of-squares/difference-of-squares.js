94dc77bc329d6300bf4835560c22666472c4f92a
Thuyen Code <thuyencode@proton.me>
2024-02-11 11:31:58 +0000

    Submit solution: javascript/difference-of-squares

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -2,21 +2,44 @@
 // This is only a SKELETON file for the 'Difference Of Squares' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// Based on my solution for this exercise on the C track: https://exercism.org/tracks/c/exercises/difference-of-squares
 
 export class Squares {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  /**
+   * @type {number | null}
+   */
+  #sumOfSquares = null
+  /**
+   * @type {number | null}
+   */
+  #squareOfSum = null
+
+  /**
+   * @param {number} number
+   */
+  constructor(number) {
+    const range = Object.freeze(
+      Array.from({ length: number }, (_, index) => index + 1)
+    )
+
+    this.#sumOfSquares = range.reduce((acc, item) => acc + Math.pow(item, 2), 0)
+    this.#squareOfSum = Math.pow(
+      range.reduce((acc, item) => acc + item, 0),
+      2
+    )
   }
 
   get sumOfSquares() {
-    throw new Error('Remove this statement and implement this function');
+    return this.#sumOfSquares
   }
 
   get squareOfSum() {
-    throw new Error('Remove this statement and implement this function');
+    return this.#squareOfSum
   }
 
   get difference() {
-    throw new Error('Remove this statement and implement this function');
+    return this.#squareOfSum - this.#sumOfSquares
   }
 }
+
+const n = new Squares(2)
