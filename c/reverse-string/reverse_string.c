5e96b68e024bd843527c853c78f740418c40ec6d
Thuyen Code <thuyencode@proton.me>
2024-02-14 08:51:14 +0000

    Submit solution: c/reverse-string

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -1,1 +1,14 @@
 #include "reverse_string.h"
+
+char *reverse(const char *value)
+{
+  const int len = strlen(value);
+  char *reversed = malloc(len + 1);
+
+  for (int i = 0, j = len - 1; j >= 0; i++, j--)
+    reversed[i] = value[j];
+
+  reversed[len] = '\0';
+
+  return reversed;
+}
--- c/reverse-string/reverse_string.h
+++ c/reverse-string/reverse_string.h
@@ -1,5 +1,7 @@
 #ifndef REVERSE_STRING_H
 #define REVERSE_STRING_H
+#include <stdlib.h>
+#include <string.h>
 
 char *reverse(const char *value);
 
