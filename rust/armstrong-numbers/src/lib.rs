a8a3a06728e2d3803999def79fdc92eadb891503
Thuyen Code <thuyencode@proton.me>
2024-02-15 03:09:36 +0000

    Change solution: rust/armstrong-numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,28 +1,15 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let mut temp: u64 = u64::from(num);
-    let mut number_of_digit: i32 = 0;
-
-    while temp != 0 {
-        temp /= 10;
-        number_of_digit += 1;
-    }
+    let num: u64 = u64::from(num);
+    let digits: String = num.to_string();
+    let number_of_digits: u32 = digits.len() as u32;
 
     let mut sum: u64 = 0;
-    let mut i: u64 = u64::from(num);
 
-    while i != 0 {
-        let num: u64 = i % 10;
-        let mut n: u64 = 1;
+    for char in digits.chars() {
+        let digit: u64 = char.to_digit(10).unwrap().into();
 
-        for _j in 0..number_of_digit {
-            n *= num;
-        }
-
-        sum += n;
-        i /= 10;
+        sum += digit.pow(number_of_digits);
     }
 
-    sum == u64::from(num)
+    sum == num
 }
-
-// Based on my solution at the C track: https://exercism.org/tracks/c/exercises/armstrong-numbers
