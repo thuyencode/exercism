4fa272cebf93624dd6b425c441c037ca435cb76e
Thuyen Code <thuyencode@proton.me>
2024-02-15 09:39:57 +0000

    Change solution: rust/armstrong-numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,15 +1,8 @@
-pub fn is_armstrong_number(num: u32) -> bool {
-    let num: u64 = u64::from(num);
+pub fn is_armstrong_number(num: u64) -> bool {
     let digits: String = num.to_string();
     let number_of_digits: u32 = digits.len() as u32;
 
-    let mut sum: u64 = 0;
-
-    for char in digits.chars() {
-        let digit: u64 = char.to_digit(10).unwrap().into();
-
-        sum += digit.pow(number_of_digits);
-    }
-
-    sum == num
+    num == digits.chars().fold(0, |acc: u64, char: char| {
+        (char.to_digit(10).unwrap() as u64).pow(number_of_digits) + acc
+    })
 }
