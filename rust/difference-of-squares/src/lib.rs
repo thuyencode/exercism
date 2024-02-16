c0ba9dae4467cf15ccb5cb893b8355572de0c323
Thuyen Code <thuyencode@proton.me>
2024-02-16 09:58:58 +0000

    Submit solution: rust/difference-of-squares

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,11 +1,25 @@
+// Based on my solution on the C track: https://exercism.org/tracks/c/exercises/difference-of-squares
+
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    let mut sum: u32 = 0;
+
+    for i in 0..=n {
+        sum += i;
+    }
+
+    sum.pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+    let mut sum: u32 = 0;
+
+    for i in 0..=n {
+        sum += i.pow(2);
+    }
+
+    sum
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
