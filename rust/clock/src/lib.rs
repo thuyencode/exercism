2efa66da14e582cbf8d77c0b0321323d2ba80800
Thuyen Code <thuyencode@proton.me>
2024-02-16 04:28:50 +0000

    Submit solution: rust/clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,11 +1,47 @@
-pub struct Clock;
+// Based on my solution on the JavaScript track: https://exercism.org/tracks/javascript/exercises/clock
+use std::fmt::{Display, Formatter, Result};
+
+#[derive(Debug, PartialEq)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Self {
+            hours: Clock::convert_to_hours(hours, minutes),
+            minutes: Clock::convert_to_minutes(minutes),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+
+    fn convert_to_minutes(minutes: i32) -> i32 {
+        let temp_for_minutes: i32 = minutes % 60;
+
+        if temp_for_minutes < 0 {
+            60 + temp_for_minutes
+        } else {
+            temp_for_minutes
+        }
+    }
+
+    fn convert_to_hours(hours: i32, minutes: i32) -> i32 {
+        let temp_for_hours: i32 = (hours + (f64::from(minutes) / 60_f64).floor() as i32) % 24;
+
+        if temp_for_hours < 0 {
+            24 + temp_for_hours
+        } else {
+            temp_for_hours
+        }
+    }
+}
+
+impl Display for Clock {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
