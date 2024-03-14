fbb6234ffa8a0351dace1dd72dca9dd3254ac977
Thuyen Code <thuyencode@proton.me>
2024-03-14 08:25:08 +0000

    Submit solution: rust/space-age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -1,12 +1,17 @@
 // The code below is a stub. Just enough to satisfy the compiler.
 // In order to pass the tests you can add-to or change any of this code.
+// Based on my solution at the JavaScript track: https://exercism.org/tracks/javascript/exercises/space-age/solutions/thuyencode
+
+const A_YEAR_ON_EARTH: f64 = 31557600.0;
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        todo!("s, measured in seconds: {s}")
+        Self { seconds: s }
     }
 }
 
@@ -25,11 +30,64 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+    fn years_during(d: &Duration) -> f64 {
+        format!("{:.2}", d.seconds as f64 / (0.2408467 * A_YEAR_ON_EARTH))
+            .parse()
+            .unwrap()
+    }
+}
+impl Planet for Venus {
+    fn years_during(d: &Duration) -> f64 {
+        format!("{:.2}", d.seconds as f64 / (0.61519726 * A_YEAR_ON_EARTH))
+            .parse()
+            .unwrap()
+    }
+}
+
+impl Planet for Earth {
+    fn years_during(d: &Duration) -> f64 {
+        format!("{:.2}", d.seconds as f64 / (1.0 * A_YEAR_ON_EARTH))
+            .parse()
+            .unwrap()
+    }
+}
+
+impl Planet for Mars {
+    fn years_during(d: &Duration) -> f64 {
+        format!("{:.2}", d.seconds as f64 / (1.8808158 * A_YEAR_ON_EARTH))
+            .parse()
+            .unwrap()
+    }
+}
+
+impl Planet for Jupiter {
+    fn years_during(d: &Duration) -> f64 {
+        format!("{:.2}", d.seconds as f64 / (11.862615 * A_YEAR_ON_EARTH))
+            .parse()
+            .unwrap()
+    }
+}
+impl Planet for Saturn {
+    fn years_during(d: &Duration) -> f64 {
+        format!("{:.2}", d.seconds as f64 / (29.447498 * A_YEAR_ON_EARTH))
+            .parse()
+            .unwrap()
+    }
+}
+
+impl Planet for Uranus {
+    fn years_during(d: &Duration) -> f64 {
+        format!("{:.2}", d.seconds as f64 / (84.016846 * A_YEAR_ON_EARTH))
+            .parse()
+            .unwrap()
+    }
+}
+
+impl Planet for Neptune {
+    fn years_during(d: &Duration) -> f64 {
+        format!("{:.2}", d.seconds as f64 / (164.79132 * A_YEAR_ON_EARTH))
+            .parse()
+            .unwrap()
+    }
+}
