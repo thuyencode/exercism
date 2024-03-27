982523de00d48402f432b830e1dd6422be7dc8d3
Thuyen Code <thuyencode@proton.me>
2024-03-27 03:57:12 +0000

    Submit solution: bash/bob

--- bash/bob/bob.sh
+++ bash/bob/bob.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
+# Thanks to: https://exercism.org/tracks/bash/exercises/bob/solutions/paulfioravanti
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+main() {
+  local -r message=$(echo "$1" | tr -d "[:space:]")
+
+  if silence; then
+    echo "Fine. Be that way!"
+  elif yelling && question; then
+    echo "Calm down, I know what I'm doing!"
+  elif yelling; then
+    echo "Whoa, chill out!"
+  elif question; then
+    echo "Sure."
+  else
+    echo "Whatever."
+  fi
+}
+
+silence() {
+  [[ -z $message ]]
+}
+
+yelling() {
+  [[ $message == *[[:upper:]]* && $message != *[[:lower:]]* ]]
+}
+
+question() {
+  [[ $message =~ \?$ ]]
+}
+
+main "$@"
