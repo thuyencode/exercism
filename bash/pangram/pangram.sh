cf7a259f4cd479254293d4be17845d11e7c9f52c
Thuyen Code <thuyencode@proton.me>
2024-03-27 03:20:28 +0000

    Submit solution: bash/pangram

--- bash/pangram/pangram.sh
+++ bash/pangram/pangram.sh
@@ -13,12 +13,21 @@
 #   # other functions here
 #   # ...
 #   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+
+main() {
+  # your main function code here
+  SENTENCE=${1,,}
+  LETTERS=("a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
+
+  for LETTER in "${LETTERS[@]}"; do
+    if [[ ! $SENTENCE =~ $LETTER ]]; then
+      echo "false"
+      exit 0
+    fi
+  done
+
+  echo "true"
+}
+
+# call main with all of the positional arguments
+main "$@"
