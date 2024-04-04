31964c9dbc9792dd0a0a52108acf160fa26f9d46
Thuyen Code <thuyencode@proton.me>
2024-04-04 15:49:38 +0000

    Submit solution: java/cars-assemble

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,25 @@
 public class CarsAssemble {
+    private double successRate(int speed) {
+        if (speed == 0) {
+            return 0;
+        } else if (speed <= 4) {
+            return 1;
+        } else if (speed <= 8) {
+            return 0.9;
+        } else if (speed == 9) {
+            return 0.8;
+        } else if (speed == 10) {
+            return 0.77;
+        } else {
+            return 0;
+        }
+    }
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+        return 221 * speed * successRate(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) (productionRatePerHour(speed) / 60);
     }
 }
