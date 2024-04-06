3cb7178ccace31412303998b841a9b3f19f531c0
Thuyen Code <thuyencode@proton.me>
2024-04-06 09:31:55 +0000

    Change solution: java/tim-from-marketing

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,9 +1,7 @@
 class Badge {
     public String print(Integer id, String name, String department) {
-        if (id == null) {
-            return String.format("%s - %s", name, department != null ? department.toUpperCase() : "OWNER");
-        }
-
-        return String.format("[%s] - %s - %s", id, name, department != null ? department.toUpperCase() : "OWNER");
+        return String.format("%s%s - %s",
+                id != null ? String.format("[%d] - ", id) : "", name,
+                department != null ? department.toUpperCase() : "OWNER");
     }
 }
