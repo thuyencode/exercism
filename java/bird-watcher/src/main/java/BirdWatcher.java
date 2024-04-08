5c7f9e70e0e159deb4102991a1b7a814610b2066
Thuyen Code <thuyencode@proton.me>
2024-04-08 14:53:09 +0000

    Fix solution: java/bird-watcher
    Convert the `long` datetype to `int`

--- java/bird-watcher/src/main/java/BirdWatcher.java
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -28,6 +28,6 @@ class BirdWatcher {
     }
 
     public int getBusyDays() {
-        return Arrays.stream(birdsPerDay).filter(n -> n >= 5).count();
+        return (int) Arrays.stream(birdsPerDay).filter(n -> n >= 5).count();
     }
 }
