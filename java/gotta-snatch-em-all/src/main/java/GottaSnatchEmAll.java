89c768b043cb75a4cc30ebedc7f7f6784756d324
Thuyen Code <thuyencode@proton.me>
2024-04-23 04:40:06 +0000

    Submit solution: java/gotta-snatch-em-all

--- java/gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
+++ java/gotta-snatch-em-all/src/main/java/GottaSnatchEmAll.java
@@ -1,25 +1,43 @@
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 class GottaSnatchEmAll {
 
     static Set<String> newCollection(List<String> cards) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.newCollection() method");
+        return new HashSet<>(cards);
     }
 
     static boolean addCard(String card, Set<String> collection) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.addCard() method");
+        return collection.add(card);
     }
 
     static boolean canTrade(Set<String> myCollection, Set<String> theirCollection) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.canTrade() method");
+        if (myCollection.isEmpty())
+            return false;
+
+        if (theirCollection.isEmpty())
+            return false;
+
+        if (myCollection.containsAll(theirCollection)) {
+            return false;
+        }
+
+        return !myCollection.equals(theirCollection);
     }
 
     static Set<String> commonCards(List<Set<String>> collections) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.commonCards() method");
+        Set<String> theCommonCards = new HashSet<>(collections.get(0));
+
+        collections.forEach((collection) -> {
+            theCommonCards.retainAll(collection);
+        });
+
+        return theCommonCards;
     }
 
     static Set<String> allCards(List<Set<String>> collections) {
-        throw new UnsupportedOperationException("Please implement the (static) GottaSnatchEmAll.allCards() method");
+        return new HashSet<>(collections.stream().flatMap(Set::stream).collect(Collectors.toList()));
     }
 }
