abf693518de897462ebe486859569900c49215ed
Thuyen Code <thuyencode@proton.me>
2024-04-23 08:57:27 +0000

    Submit solution: java/logs-logs-logs

--- java/logs-logs-logs/src/main/java/LogLine.java
+++ java/logs-logs-logs/src/main/java/LogLine.java
@@ -1,13 +1,51 @@
 public class LogLine {
+    private String logMessage;
+    private LogLevel logLevel;
 
     public LogLine(String logLine) {
+        this.logMessage = logLine.substring(logLine.indexOf(':') + 2).strip();
+
+        System.out.println(logLine.substring(logLine.indexOf('[') + 1, logLine.indexOf(']')));
+        switch (logLine.substring(logLine.indexOf('[') + 1, logLine.indexOf(']'))) {
+            case "TRC":
+                this.logLevel = LogLevel.TRACE;
+                break;
+
+            case "DBG":
+                this.logLevel = LogLevel.DEBUG;
+                break;
+
+            case "INF":
+                this.logLevel = LogLevel.INFO;
+                break;
+
+            case "WRN":
+                this.logLevel = LogLevel.WARNING;
+                break;
+
+            case "ERR":
+                this.logLevel = LogLevel.ERROR;
+                break;
+
+            case "FTL":
+                this.logLevel = LogLevel.FATAL;
+                break;
+
+            default:
+                this.logLevel = LogLevel.UNKNOWN;
+                break;
+        }
     }
 
     public LogLevel getLogLevel() {
-        throw new UnsupportedOperationException("Please implement the getLogLevel() method");
+        return this.logLevel;
     }
 
     public String getOutputForShortLog() {
-        throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+        return String.format("%d:%s", this.logLevel.getEncodedLogLevel(), this.logMessage);
+    }
+
+    public static void main(String[] args) {
+        new LogLine("[DBG]: ; expected");
     }
 }
