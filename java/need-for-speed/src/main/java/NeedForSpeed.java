4ae7e6ea09e5a79031e3529e2b17add64087ece5
Thuyen Code <thuyencode@proton.me>
2024-04-23 02:20:29 +0000

    Submit solution: java/need-for-speed

--- java/need-for-speed/src/main/java/NeedForSpeed.java
+++ java/need-for-speed/src/main/java/NeedForSpeed.java
@@ -1,31 +1,50 @@
 class NeedForSpeed {
+    private int distanceDriven;
+    private int battery;
+    private int speed;
+    private int batteryDrain;
+
     NeedForSpeed(int speed, int batteryDrain) {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed constructor");
+        this.speed = speed;
+        this.batteryDrain = batteryDrain;
+        this.distanceDriven = 0;
+        this.battery = 100;
     }
 
     public boolean batteryDrained() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.batteryDrained() method");
+        return this.battery < this.batteryDrain;
     }
 
     public int distanceDriven() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.distanceDriven() method");
+        return this.distanceDriven;
     }
 
     public void drive() {
-        throw new UnsupportedOperationException("Please implement the NeedForSpeed.drive() method");
+        if (this.batteryDrained()) {
+            return;
+        }
+
+        this.distanceDriven += this.speed;
+        this.battery -= this.batteryDrain;
     }
 
     public static NeedForSpeed nitro() {
-        throw new UnsupportedOperationException("Please implement the (static) NeedForSpeed.nitro() method");
+        return new NeedForSpeed(50, 4);
     }
 }
 
 class RaceTrack {
+    private int distance;
+
     RaceTrack(int distance) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack constructor");
+        this.distance = distance;
     }
 
     public boolean tryFinishTrack(NeedForSpeed car) {
-        throw new UnsupportedOperationException("Please implement the RaceTrack.tryFinishTrack() method");
+        while (!car.batteryDrained()) {
+            car.drive();
+        }
+
+        return car.distanceDriven() >= this.distance;
     }
 }
