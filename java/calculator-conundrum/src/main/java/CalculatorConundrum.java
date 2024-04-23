7b65ef23f11fd5ff6a6193ae5bd55805e7cf5b13
Thuyen Code <thuyencode@proton.me>
2024-04-23 03:59:48 +0000

    New exercise: java/calculator-conundrum

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,29 @@
 class CalculatorConundrum {
-    public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+    public String calculate(int operand1, int operand2, String operation)
+            throws IllegalOperationException, IllegalArgumentException {
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        }
+
+        try {
+            switch (operation) {
+                case "+":
+                    return String.format("%d %s %d = %d", operand1, operation, operand2, operand1 + operand2);
+
+                case "*":
+                    return String.format("%d %s %d = %d", operand1, operation, operand2, operand1 * operand2);
+
+                case "/":
+                    return String.format("%d %s %d = %d", operand1, operation, operand2, operand1 / operand2);
+
+                case "":
+                    throw new IllegalArgumentException("Operation cannot be empty");
+
+                default:
+                    throw new IllegalOperationException(String.format("Operation '%s' does not exist", operation));
+            }
+        } catch (ArithmeticException e) {
+            throw new IllegalOperationException("Division by zero is not allowed", e);
+        }
     }
 }
