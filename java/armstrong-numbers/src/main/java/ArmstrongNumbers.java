cb3c42b75217fc78ca2b788874004a197acf849d
Thuyen Code <thuyencode@proton.me>
2024-04-26 14:51:47 +0000

    Submit solution: java/armstrong-numbers

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,28 @@
 class ArmstrongNumbers {
-
     boolean isArmstrongNumber(int numberToCheck) {
+        int temp = numberToCheck;
+        int number_of_digits = 0;
 
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        // Calculate the number on digits in `numberToCheck`
+        while (temp != 0) {
+            temp /= 10;
+            number_of_digits++;
+        }
 
+        int sum = 0;
+
+        for (int i = numberToCheck; i != 0; i /= 10) {
+            int num = i % 10;
+            int n = 1;
+
+            for (int j = 0; j < number_of_digits; j++) {
+                n *= num;
+            }
+
+            sum += n;
+        }
+
+        return sum == numberToCheck;
     }
 
 }
