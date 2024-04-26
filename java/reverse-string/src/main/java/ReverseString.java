d50c650fc7852250ced6535e116888e134255784
Thuyen Code <thuyencode@proton.me>
2024-04-26 14:26:48 +0000

    Submit solution: java/reverse-string

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,5 @@
 class ReverseString {
-
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return new StringBuilder(inputString).reverse().toString();
     }
-  
 }
